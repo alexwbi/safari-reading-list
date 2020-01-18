29d128aa5283c14baaee02d6c81fbecd79c9fc0d
Alex Bi <alexwbi95@gmail.com>
2020-01-18 14:25:01 -0500

    Refactor

--- reading_list_urls.py
+++ reading_list_urls.py
@@ -4,6 +4,10 @@ BOOKMARKS_PATH = 'Library/Safari/Bookmarks.plist'
 
 
 def get_reading_list_urls():
-    with open(BOOKMARKS_PATH, 'rb') as f:
-        bookmarks = plistlib.load(f)
+    bookmarks = _read_file()
     pass
+
+
+def _read_file():
+    with open(BOOKMARKS_PATH, 'rb') as f:
+        return plistlib.load(f)
