093ef4339c6e51b4acb24238a0e895d5b45d6c4b
Alex Bi <alexwbi95@gmail.com>
2020-01-18 14:29:02 -0500

    Get reading list from bookmarks

--- reading_list_urls.py
+++ reading_list_urls.py
@@ -1,13 +1,19 @@
 import plistlib
 
 BOOKMARKS_PATH = 'Library/Safari/Bookmarks.plist'
+READING_LIST_KEY = 'com.apple.ReadingList'
 
 
 def get_reading_list_urls():
     bookmarks = _read_file()
+    reading_list = _reading_list(bookmarks)
     pass
 
 
 def _read_file():
     with open(BOOKMARKS_PATH, 'rb') as f:
         return plistlib.load(f)
+
+
+def _reading_list(bookmarks):
+    return list(filter(lambda item: item['Title'] == READING_LIST_KEY, bookmarks['Children']))[0]
